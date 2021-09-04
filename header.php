70e84c9dc0809d742da547c890608fd9646ba284
Kenna <65411099+KennjiNemera@users.noreply.github.com>
2021-09-05 00:22:35 +0200

    Initial Upload

---
+++ header.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset='utf-8'>
+  <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+  <title>Ferdinand Postma High School</title>
+  <meta name='viewport' content='width=device-width, initial-scale=1'>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+  <link rel='stylesheet' type='text/css' media='screen' href='style.css'>
+  <link rel="stylesheet" href="fontawesome-free-5.15.4-web/css/all.css">
+  <link rel="stylesheet" href="fonts/icomoon/style.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+
+<body>
+
+  <!-- NAVBAR -->
+
+  <div class="container-fluid topnavbar">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-sm-3">
+          <i class="fa fa-phone" aria-hidden="true"></i>
+          <a class="contactinfo">018 290 5539</a>
+        </div>
+        <div class="col-sm-3">
+          <i class="fa fa-envelope" aria-hidden="true"></i>
+          <a class="contactinfo">ontvangs@ferdies.co.za</a>
+        </div>
+        <div class="col-sm-3">
+          <i class="fa fa-map-marker" aria-hidden="true"></i>
+          <a class="contactinfo">20 Lanyon Street, Potchefstroom</a>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid" style="width: 90%;">
+      <a class="navbar-brand" href="#"><img src="assets/logo.png" class="nav-logo"></a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Academics
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">NSC Results</a></li>
+              <li><a class="dropdown-item" href="#">Top 10</a></li>
+              <li><a class="dropdown-item" href="#">Olympiads</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Admission
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Procedure</a></li>
+              <li><a class="dropdown-item" href="#">Fees</a></li>
+              <li><a class="dropdown-item" href="#">Application Forms</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Sport
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Hockey</a></li>
+              <li><a class="dropdown-item" href="#">Rugby</a></li>
+              <li><a class="dropdown-item" href="#">Netball</a></li>
+              <li><a class="dropdown-item" href="#">Athletics</a></li>
+              <li><a class="dropdown-item" href="#">Cross country</a></li>
+              <li><a class="dropdown-item" href="#">Cricket</a></li>
+              <li><a class="dropdown-item" href="#">Tennis</a></li>
+              <li><a class="dropdown-item" href="#">Squash</a></li>
+              <li><a class="dropdown-item" href="#">Shooting</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Leadership
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Procedure</a></li>
+              <li><a class="dropdown-item" href="#">Fees</a></li>
+              <li><a class="dropdown-item" href="#">Application Forms</a></li>
+            </ul>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown"
+              aria-expanded="false">
+              Charity
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Procedure</a></li>
+              <li><a class="dropdown-item" href="#">Fees</a></li>
+              <li><a class="dropdown-item" href="#">Application Forms</a></li>
+            </ul>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
